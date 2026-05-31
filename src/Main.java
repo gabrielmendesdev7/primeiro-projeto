83b0defbf86d9cd3d8ea007e258b160a9257d7fa
gabrielmendesdev7 <andrewsseven@gmail.com>
2026-05-31 15:37:46 -0300

    expressoes comparativas

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int idade = 35;
+        double salarioMensal = 4500.00;
+        char genero = 'M';
+        boolean empregadoJava = true;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário Mensal: " + salarioMensal);
+        System.out.println("Gênero: " + genero);
+        System.out.println("Empregado em Java: " + empregadoJava);
+    }
+}
