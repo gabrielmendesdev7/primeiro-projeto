3c1dd794972783fcbd0fdcbd8110ad75328b4192
gabrielmendesdev7 <andrewsseven@gmail.com>
2026-07-09 17:38:41 -0300

    if-else

---
+++ src/EstruturaIfElse.java
@@ -0,0 +1,24 @@
+public class EstruturaIfElse {
+
+    public static void main(String[] args) {
+    /*    int idade = 1;
+
+        if(idade >= 18) {
+            System.out.println("Você é maior de idade.");
+        } else {
+            System.out.println("Você é menor de idade.");
+        }*/
+
+        boolean temDinheiro = true;
+        boolean temCartao = false;
+
+        if(temDinheiro && temCartao) {
+            System.out.println("Pede um ifood e um zé delivery");
+        } else if(temDinheiro || temCartao) {
+            System.out.println("Pede um ifood");
+        } else {
+            System.out.println("Não pede nada");
+        }
+    }
+
+}
