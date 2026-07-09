b98bac949a73c635d9a866056d869727913c4849
gabrielmendesdev7 <andrewsseven@gmail.com>
2026-07-09 18:08:49 -0300

    while

---
+++ src/While.java
@@ -0,0 +1,12 @@
+public class While {
+
+    public static void main(String[] args) {
+        int contador = 0;
+
+        while (contador <= 5) {
+            System.out.println("Contador: " + contador);
+            contador++;
+        }
+    }
+
+}
