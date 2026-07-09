365ddcdc35829a990c75cf896bd2e473ef8ffb39
gabrielmendesdev7 <andrewsseven@gmail.com>
2026-07-09 18:13:58 -0300

    do-while

--- src/While.java
+++ src/While.java
@@ -3,10 +3,15 @@ public class While {
     public static void main(String[] args) {
         int contador = 0;
 
-        while (contador <= 5) {
+     /*   while (contador <= 5) {
             System.out.println("Contador: " + contador);
             contador++;
-        }
+        }*/
+
+        do {
+            System.out.println("Contador do while " + contador);
+            contador++;
+        } while(contador < 5);
     }
 
 }
