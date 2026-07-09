4f3e01d8481b98d2ff3afcce40ff5955cadd78da
gabrielmendesdev7 <andrewsseven@gmail.com>
2026-07-09 18:20:52 -0300

    for

--- src/While.java
+++ src/While.java
@@ -1,17 +1,21 @@
 public class While {
 
     public static void main(String[] args) {
-        int contador = 0;
+       // int contador = 0;
 
      /*   while (contador <= 5) {
             System.out.println("Contador: " + contador);
             contador++;
         }*/
 
-        do {
+ /*       do {
             System.out.println("Contador do while " + contador);
             contador++;
-        } while(contador < 5);
+        } while(contador < 5);*/
+
+        for(int cont = 0; cont < 5; cont++) {
+            System.out.println("Contador for: " + cont);
+        }
     }
 
 }
