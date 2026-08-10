db808eff4645d10d691018f5bc96fe1b8244c2e8
gabrielmendesdev7 <andrewsseven@gmail.com>
2026-08-10 17:37:17 -0300

    Métodos de Instância e Métodos Estáticos

---
+++ src/Execucao.java
@@ -0,0 +1,10 @@
+public class Execucao {
+
+    public static void main(String[] args) {
+        Integer cpf = 01234;
+
+        String cpfString = Util.conversorString(cpf);
+        System.out.println("CPF em String: " + cpfString);
+    }
+
+}
