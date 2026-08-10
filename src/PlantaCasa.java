96fa0c3e7afcdd5219bf5ecf6558396ffec5660b
gabrielmendesdev7 <andrewsseven@gmail.com>
2026-08-10 17:23:21 -0300

    Métodos: Parâmetros de Métodos

--- src/PlantaCasa.java
+++ src/PlantaCasa.java
@@ -17,4 +17,52 @@ public class PlantaCasa {
         System.out.println("Cor " + cor);
     }
 
+    public void mudarCorParede(String novaCor) {
+        cor = novaCor;
+        pintar();
+    }
+
+    public int somarMetragem(){
+        return metragem * numeroBanheiros + numeroQuartos;
+    }
+
+    public void alterarCaracteristicas(int metros, int quartos, int banheiro, String mat){
+        metragem = metros;
+        numeroQuartos = quartos;
+        numeroBanheiros = banheiro;
+        material = mat;
+        construir();
+    }
+
+    public static void main(String[] args) {
+        PlantaCasa casa = new PlantaCasa();
+
+        casa.numeroBanheiros = 2;
+        casa.numeroQuartos = 3;
+        casa.metragem = 70;
+        casa.material = "Tijolo";
+        casa.cor = "Cinza";
+
+        casa.construir();
+
+        casa.pintar();
+
+        casa.mudarCorParede("Branco");
+
+        casa.alterarCaracteristicas(100, 2, 2, "Madeira");
+
+        int resultado = casa.somarMetragem();
+        System.out.println("Resultado da soma da metragem: " + resultado);
+
+        PlantaCasa casaVizinho = new PlantaCasa();
+        casaVizinho.metragem = 200;
+        casaVizinho.material = "Alvenaria";
+        casaVizinho.numeroBanheiros = 1;
+        casaVizinho.numeroQuartos = 1;
+        casaVizinho.cor = "Amarela";
+
+        casaVizinho.construir();
+        casaVizinho.pintar();
+    }
+
 }
